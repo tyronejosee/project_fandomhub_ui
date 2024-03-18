d8bc07038e6f53194e86e4a34e1e6fa27fe1a8b7
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-03-18 13:35:31 -0300

    feat(project): add structure basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,36 @@
-import Card from "./components/Card";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import HelpPage from "./pages/HelpPage";
+import Layout from "./components/layout/Layout";
+import AnimeDetail from "./components/layout/AnimeDetail";
+import Pending from "./components/common/Pending";
 
 function App() {
   return (
-    <>
-      <main className="max-w-screen-lg mx-auto p-4">
-        <section className="grid grid-cols-4 gap-4">
-          <Card
-            title="Jujutsu Kaisen"
-            description="Esta es una tarjeta de ejemplo"
-            image="https://via.placeholder.com/300"
-          />
-          <Card
-            title="Naruto"
-            description="Esta es una tarjeta de ejemplo"
-            image="https://via.placeholder.com/300"
-          />
-          <Card
-            title="Dragon Ball Z"
-            description="Esta es una tarjeta de ejemplo"
-            image="https://via.placeholder.com/300"
-          />
-          <Card
-            title="Elfen Died"
-            description="Esta es una tarjeta de ejemplo"
-            image="https://via.placeholder.com/300"
-          />
-            <Card
-            title="Elfen Died"
-            description="Esta es una tarjeta de ejemplo"
-            image="https://via.placeholder.com/300"
-          />
-            <Card
-            title="Elfen Died"
-            description="Esta es una tarjeta de ejemplo"
-            image="https://via.placeholder.com/300"
-          />
-        </section>
-      </main>
-    </>
+    <Router>
+      <Layout>
+        <Routes>
+          {/* Main paths */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<HelpPage />} />
+          <Route path="/article" element={<Pending />} />
+          {/* Help paths */}
+          <Route path="/help/support" element={<Pending />} />
+          <Route path="/help/advertising" element={<Pending />} />
+          <Route path="/help/faq" element={<Pending />} />
+          <Route path="/help/terms" element={<Pending />} />
+          <Route path="/help/privacy" element={<Pending />} />
+          <Route path="/help/cookie" element={<Pending />} />
+          <Route path="/help/notice-at-collection" element={<Pending />} />
+          <Route path="/sitemap" element={<Pending />} />
+          <Route path="/animes" element={<Pending />} />
+          <Route path="/animes/:id" element={<AnimeDetail />} />
+          <Route path="/mangas" element={<Pending />} />
+          {/* Error paths */}
+          <Route path="*" element={<Pending />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
