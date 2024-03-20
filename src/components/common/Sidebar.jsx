6d5fc5c56e2bc6f280e6c680c2607c36124a3590
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-03-20 19:52:21 -0300

    feat(components): add rankinglist component, update sidebar

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,52 +1,25 @@
+import RankingList from "../ranking/RankingList";
+
 const Sidebar = () => {
   return (
-    <aside className="hidden lg:block fixed z-20 inset-0 top-[110px] left-[max(0px,calc(50%-45rem))] right-auto w-[19rem] pb-10 pl-8 pr-6 overflow-y-auto border-r">
-      <section className="">
-        <h2 className="text-center text-2xl font-medium">Top Airing Anime</h2>
-        <span className="text-xs">More</span>
-        <p className="text-balance leading-snug">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
-          fugiat porro corrupti dignissimos illo assumenda ut quasi voluptates
-          rerum aliquid mollitia quis, nisi fuga quibusdam rem aut debitis nemo
-          repudiandae. Numquam culpa excepturi velit rem temporibus aut nulla
-          ipsum itaque veniam deserunt dolor, rerum sapiente soluta odit
-          praesentium aperiam laborum vitae possimus ex eos quo. Voluptatum
-          repellat neque quaerat necessitatibus! Iste dignissimos fugit
-          doloribus aspernatur aperiam enim totam itaque, maiores vero nisi
-          impedit consequuntur illo commodi. Atque ab quod facere expedita quos
-          totam vitae repellat ullam, nesciunt sequi quas at! Reprehenderit
-          expedita repudiandae quae doloremque corrupti distinctio placeat porro
-          molestias delectus magnam. Veritatis facilis non sunt amet, dolores ut
-          corrupti temporibus perspiciatis quasi quaerat enim aliquam aut quas
-          modi cum? Culpa, repellat nobis? Exercitationem maiores aperiam
-          eligendi consequatur. Similique modi ab natus nihil incidunt
-          asperiores dolorem voluptatibus fugiat voluptatum, voluptates vero
-          perspiciatis hic officiis ut recusandae ea sit mollitia id. Quam,
-          accusamus atque fugiat eligendi vitae mollitia harum dignissimos
-          eaque. Ea facilis cum enim pariatur dolore sint vitae aliquid laborum
-          totam perspiciatis deserunt necessitatibus, iure velit? Ipsam corporis
-          earum quidem. Repellat expedita accusantium nobis quia in quos,
-          laborum rerum suscipit corporis. Culpa natus placeat magni ad dolores
-          mollitia similique eum repellendus animi quibusdam. Autem ipsam,
-          ratione vel est quam recusandae. Sequi, architecto beatae itaque
-          laudantium facere adipisci quidem eaque accusantium accusamus iusto
-          quod quam modi obcaecati sunt natus sit facilis deleniti. Ipsa,
-          dolores unde! Assumenda molestias recusandae culpa iusto asperiores.
-          Tenetur, ipsa esse. Unde harum eligendi ad recusandae labore quisquam
-          enim pariatur eum dolore suscipit perferendis iusto, doloribus, quis
-          vero obcaecati dignissimos modi dolorem sit sapiente! Voluptatum earum
-          in reiciendis? Saepe dolor magni laudantium labore vitae vero atque
-          facere architecto exercitationem? Quod natus numquam asperiores rem!
-          Fugiat maiores corporis, quos nisi suscipit sint! Perferendis sint
-          culpa nobis atque in! Quidem? Similique, tempora! At eum reiciendis,
-          architecto sint tempore esse voluptate totam saepe molestiae iure
-          laboriosam necessitatibus, neque aliquam mollitia explicabo asperiores
-          ullam cupiditate cumque alias? Repudiandae harum provident et tempora?
-          Praesentium, ipsa soluta tempore quisquam ratione id magnam nihil,
-          ipsam fuga ut sequi eum minus quaerat. Mollitia excepturi in quidem,
-          sed quis exercitationem voluptas dolore accusantium id debitis
-          suscipit eos?
-        </p>
+    <aside className="hidden lg:block fixed z-20 inset-0 top-[110px] left-[max(0px,calc(50%-45rem))] right-auto w-[19rem] overflow-y-auto border-r dark:border-neutral-800 space-y-8">
+      <section>
+        <h2 className="text-lg font-medium">Top Airing Anime</h2>
+        <RankingList />
+        <RankingList />
+        <RankingList />
+        <RankingList />
+        <RankingList />
+        <RankingList />
+      </section>
+      <section>
+        <h2 className="text-lg font-medium">Top Airing Anime</h2>
+        <RankingList />
+        <RankingList />
+        <RankingList />
+        <RankingList />
+        <RankingList />
+        <RankingList />
       </section>
     </aside>
   );
